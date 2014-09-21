f8049be119e5a3d8bba2ae4af3756c9cc202fdb8
Cian Murphy VYP <sejjcmu@zeppo-10-20.local>
2014-09-21 18:20:59 +0100

    added run gene test script - test with both kinships is super slow

--- run_gene_tests.sh
+++ run_gene_tests.sh
@@ -7,18 +7,22 @@ data="/scratch2/vyp-scratch2/cian/UCLex_August2014/Genotype_Matrix"
 genes="/SAN/biomed/biomed14/vyp-scratch/cian/LDAK/genesldak_ref.txt"
 weights="/scratch2/vyp-scratch2/cian/UCLex_August2014/Genotype_weights/re-weightsALL" 
 
-kinship="/scratch2/vyp-scratch2/cian/UCLex_August2014/Depth_weights/kinshipALL"
+kinship="/scratch2/vyp-scratch2/cian/UCLex_August2014/TechKin_weights/kinshipALL"  ; kin="Tech"
+#kinship="/scratch2/vyp-scratch2/cian/UCLex_August2014/Depth_weights/kinshipALL"   ; kin="Depth"
 
 
-phenotypes="/cluster/project8/vyp/cian/data/UCLex/UCLex_April2014c/mainset/UCLex_AprilC_All_phenotypes_for_fastLMM"
+phenotypes="/scratch2/vyp-scratch2/cian/UCLex_August2014/All_phenotypes"
 # pheno=14 # 21 is Syrris
 maf=0.00001
 missing=0.9
 
-for pheno in `seq 1 23`;
+groups=$(wc -l /cluster/project8/vyp/cian/data/UCLex/UCLex_August/Scripts/Groups | awk '{print $1}' ) 
+
+#for pheno in `seq 1 23`;
+for pheno in `seq 1 $groups`;
 	do   
 
-out=$(sed -n "$pheno"'p' /cluster/project8/vyp/cian/data/UCLex/UCLex_April2014c/mainset/Groups)
+out=$(sed -n "$pheno"'p' /cluster/project8/vyp/cian/data/UCLex/UCLex_August/Scripts/Groups)
 
 oDir="perm_no_kin_maf_"$maf"_pheno_$out"_missingness_"$missing/"
 rm -fr $oDir ; mkdir $oDir
@@ -51,7 +55,7 @@ cd $oDir ; runSh $oFile ; cd ..
 done
 
 
-oDir="perm_with_kin_maf_"$maf"_pheno_$out"_missingness_"$missing/"
+oDir=$kin"_perm_with_kin_maf_"$maf"_pheno_$out"_missingness_"$missing/"
 rm -fr $oDir ; mkdir $oDir
 #echo "$ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 " > tmp.sh ; runSh tmp.sh
 $ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 
@@ -67,7 +71,7 @@ done
 
 
 
-oDir="with_kin_maf_"$maf"_pheno_$out"_missingness_"$missing/"
+oDir=$kin"_with_kin_maf_"$maf"_pheno_$out"_missingness_"$missing/"
 rm -fr $oDir ; mkdir $oDir
 #echo "$ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 " > tmp.sh ; runSh tmp.sh
 $ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 
@@ -81,12 +85,103 @@ $ldak  --calc-genes-reml "../"$oDir --bfile $data --pheno $phenotypes --mpheno $
 cd $oDir ; runSh $oFile ; cd .. 
 done 
 
+	## Repeat gene tests with other kinship matrix. 
+	kinship="/scratch2/vyp-scratch2/cian/UCLex_August2014/Depth_weights/kinshipALL"   ; kin="Depth"
 
-# $ldak --decompose output --grm $kinship 
-# $ldak --reml ALevine --grm $kinship --pheno $phenotypes --mpheno 1 --eigen output
+
+	oDir=$kin"_perm_with_kin_maf_"$maf"_pheno_$out"_missingness_"$missing/"
+	rm -fr $oDir ; mkdir $oDir
+	#echo "$ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 " > tmp.sh ; runSh tmp.sh
+	$ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 
+	Partitions=$(tail -1 $oDir/gene_details.txt | awk '{print $2}')
+	for partition in $(seq 1 $Partitions)
+	do
+	oFile='partition_'$partition'.sh'
+	echo "
+	$ldak  --calc-genes-reml "../"$oDir --bfile $data --pheno $phenotypes --mpheno $pheno --weights $weights --minmaf $maf --minvar 0.00001 --minobs $missing --partition $partition --grm $kinship --permute YES
+	" > $oDir$oFile
+	cd $oDir ; runSh $oFile ; cd .. 
+	done
+
+
+
+	oDir=$kin"_with_kin_maf_"$maf"_pheno_$out"_missingness_"$missing/"
+	rm -fr $oDir ; mkdir $oDir
+	#echo "$ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 " > tmp.sh ; runSh tmp.sh
+	$ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 
+	Partitions=$(tail -1 $oDir/gene_details.txt | awk '{print $2}')
+	for partition in $(seq 1 $Partitions)
+	do
+	oFile='partition_'$partition'.sh'
+	echo "
+	$ldak  --calc-genes-reml "../"$oDir --bfile $data --pheno $phenotypes --mpheno $pheno --weights $weights --minmaf $maf --minvar 0.00001 --minobs $missing --partition $partition --grm $kinship 
+	" > $oDir$oFile
+	cd $oDir ; runSh $oFile ; cd .. 
+	done 
+
+
+
 
 
 done
+
+#############################################################################################################
+################### 				Gene tests with both kinships. 
+############################################################################################################
+aKinship="/scratch2/vyp-scratch2/cian/UCLex_August2014/TechKin_weights/kinshipALL"
+bKinship="/scratch2/vyp-scratch2/cian/UCLex_August2014/Depth_weights/kinshipALL"
+
+
+groups=$(wc -l /cluster/project8/vyp/cian/data/UCLex/UCLex_August/Scripts/Groups | awk '{print $1}' ) 
+
+#for pheno in `seq 1 23`;
+for pheno in `seq 1 $groups`;
+	do   
+
+out=$(sed -n "$pheno"'p' /cluster/project8/vyp/cian/data/UCLex/UCLex_August/Scripts/Groups)
+
+
+kin="Both"
+oDir=$kin"_with_kin_maf_"$maf"_pheno_$out"_missingness_"$missing/"
+rm -fr $oDir ; mkdir $oDir
+#echo "$ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 " > tmp.sh ; runSh tmp.sh
+$ldak --cut-genes $oDir --genefile $genes --bfile $data  --gene-buffer 20000 --weights $weights --partition-length 50000 
+Partitions=$(tail -1 $oDir/gene_details.txt | awk '{print $2}')
+for partition in $(seq 1 $Partitions)
+do
+oFile='partition_'$partition'.sh'
+echo "
+$ldak  --calc-genes-reml "../"$oDir --bfile $data --pheno $phenotypes --mpheno $pheno --weights $weights --minmaf $maf --minvar 0.00001 --minobs $missing --partition $partition --mgrm "../"kinships
+" > $oDir$oFile
+cd $oDir ; runSh $oFile ; cd .. 
+done 
+
+done
+
+#############################################################################################################
+################### 				How much variance does each kinship exxplain. 
+############################################################################################################
+
+echo "
+ldak="/cluster/project8/vyp/cian/support/ldak/ldak.4.5"
+
+aKinship="/scratch2/vyp-scratch2/cian/UCLex_August2014/TechKin_weights/kinshipALL"
+bKinship="/scratch2/vyp-scratch2/cian/UCLex_August2014/Depth_weights/kinshipALL"
+
+$ldak --decompose tech --grm $aKinship 		
+$ldak --reml ALevine.tech --grm $kinship --pheno $phenotypes --mpheno 1 --eigen tech
+
+
+$ldak --decompose depth --grm $bKinship 
+$ldak --reml ALevine.depth --grm $kinship --pheno $phenotypes --mpheno 1 --eigen depth
+
+
+" > decompose.kinships.sh
+# runSh decompose.kinships.sh
+
+
+
+
 
 
 
