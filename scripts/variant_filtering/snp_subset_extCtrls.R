a6ab747f941d825d2ad89a01bed0727dd44c0582
Cian Murphy <cian.murphy.11@ucl.ac.uk>
2015-02-23 19:05:57 +0000

    Updating step1

---
+++ scripts/variant_filtering/snp_subset_extCtrls.R
@@ -0,0 +1,15 @@
+getArgs <- function() {
+  myargs.list <- strsplit(grep("=",gsub("--","",commandArgs()),value=TRUE),"=")
+  myargs <- lapply(myargs.list,function(x) x[2] )
+  names(myargs) <- lapply(myargs.list,function(x) x[1])
+  return (myargs)
+}
+
+release <- 'February2015'
+
+myArgs <- getArgs()
+
+if ('rootODir' %in% names(myArgs))  rootODir <- myArgs[[ "rootODir" ]]
+if ('release' %in% names(myArgs))  release <- myArgs[[ "release" ]]
+
+
