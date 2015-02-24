c6bf5d6bcc62d4c97576c37f47ac5cddc1241aee
Cian Murphy <cian.murphy.11@ucl.ac.uk>
2015-02-24 02:03:46 +0000

    Updating step1

--- scripts/check_tk_residuals.sh
+++ scripts/check_tk_residuals.sh
@@ -23,29 +23,31 @@ tail -n +2 "tmp" > $Names
 rm tmp
 nbGroups=$(wc -l $groups | awk {'print $1}') 
 
-oDir=$bdir"KinshipDecomposition"
+oDir=$bdir"KinshipDecomposition/"
 if [ ! -e $oDir ]; then mkdir $obDir; fi
 
 for pheno in $(seq 1 $nbGroups)
 do
 	batch=$(sed -n $pheno'p' $Names)
-	$ldak --reml $batch --grm $kinship --bfile $data --pheno $phenotypes --mpheno $pheno
+	$ldak --reml $oDir$batch --grm $kinship --bfile $data --pheno $phenotypes --mpheno $pheno
 	
 	if [ $pheno=1 ] 
 	then
-		awk '{ print $1, $1, $5}' $batch".indi.res" > $bDir".NewPhenotypeFiletmp"
+		awk '{ print $1, $1, $5}' $oDir$batch".indi.res" > $bDir".NewPhenotypeFiletmp"
 	fi 
 	if [ $pheno>1 ] 
 	then
-		paste -d' ' $bDir".NewPhenotypeFiletmp" <(awk '{print $NF}' $batch".indi.res")
+		paste $bDir".NewPhenotypeFiletmp" <(awk '{print $NF}' $oDir$batch".indi.res")
 	fi 
 
 done
 
 tail -n +2 $bDir".NewPhenotypeFile" > $bDir"NewPhenotypeFile"
 
+oFile=plot.residuals.R
+echo "dir<-$oDir$batch" > $oFile
 echo '
-	files <- list.files(pattern = "res")
+	files <- list.files(dir, pattern = "res", full.names=T)
 	pdf("Residuals.pdf") 
 	par(mfrow=c(2,2))  
 	lapply(files, function(x)) 
@@ -55,6 +57,8 @@ echo '
 		with(file, plot(Phenotype, Residual), xlab = name)
 	}
 	dev.off() 
-	' > plot.residuals.R
-R CMD BATCH --no-save --no-restore plot.residuals.R
+	' >> $oFile
+R CMD BATCH --no-save --no-restore $oFile
+
+
 
