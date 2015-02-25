4926b4434949e6e8089d6babada4a871536c355e
Cian Murphy <cian.murphy.11@ucl.ac.uk>
2015-02-25 11:52:54 +0000

    Updating step1

--- scripts/LDAK/run_ldak_on_all_phenos.sh
+++ scripts/LDAK/run_ldak_on_all_phenos.sh
@@ -3,22 +3,22 @@
 shopt -s expand_aliases
 source ~/.bashrc
 
-release=October2014
+release=February2015
 
-nbGroups=$(tail -1 nb.groups | awk '{print $1}') 
-echo $nbGroups "phenotypes for LDAK to chew on"
-
+bDir=/scratch2/vyp-scratch2/cian/UCLex_${release}/
+Groups=$bDir"GroupNames"
+nbGroups=$(wc -l  $Groups | awk '{print $1}') 
 templateScript="LDAK.sh"
 
-oFolder="./LDAK_gene_tests_all_phenos"
+oFolder=$bDir"LDAK_gene_tests_all_phenos"
 rm -r $oFolder ; mkdir $oFolder
 
 for pheno in $(seq 1 $nbGroups)
 do
-	batch=$(sed -n $pheno'p' Groups)
+	batch=$(sed -n $pheno'p' $Groups)
 	target=$oFolder"/"$batch".LDAK.sh"
 	
-	echo "phenotypes='/cluster/project8/vyp/cian/data/UCLex/UCLex_${release}/All_phenotypes/All_phenotypes' ; mPheno='$pheno' ; pheno='$batch'"  > $target
+	echo "phenotypes='/scratch2/vyp-scratch2/cian/UCLex_${release}/Phenotypes' ; mPheno='$pheno' ; pheno='$batch'"  > $target
 	echo "extract='/cluster/project8/vyp/cian/data/UCLex/UCLex_${release}/All_phenotypes/LDAK/SNP.data.extract' ; role='Func'" >> $target
 	cat $templateScript >> $target
 	cd $oFolder ; sh $batch".LDAK.sh" ; cd ..
