196c906761049a26fa302cd9a6face73a8c66ff3
Cian Murphy <cian.murphy.11@ucl.ac.uk>
2015-02-26 14:05:52 +0000

    Updating step1

--- scripts/LDAK/plot_gene_test.R
+++ scripts/LDAK/plot_gene_test.R
@@ -1,31 +1,13 @@
-getArgs <- function() {
-  myargs.list <- strsplit(grep("=",gsub("--","",commandArgs()),value=TRUE),"=")
-  myargs <- lapply(myargs.list,function(x) x[2] )
-  names(myargs) <- lapply(myargs.list,function(x) x[1])
-  return (myargs)
-}
-
-release <- 'February2015'
-
-myArgs <- getArgs()
-
-if ('rootODir' %in% names(myArgs))  rootODir <- myArgs[[ "rootODir" ]]
-if ('release' %in% names(myArgs))  release <- myArgs[[ "release" ]]
-
-######################
-
 library(snpStats)
-library(GenABEL)
 
-files <- list.files(pattern = "flt_Tech") 
-batches <- gsub(files, pattern = ".*Func_", replacement = "")
+files <- list.files(pattern = "with_kin") 
+batches <- gsub(files, pattern = ".*01_", replacement = "")
 
 groups <- read.table("/cluster/project8/vyp/cian/data/UCLex/UCLex_October2014/All_phenotypes/Nb_per_group", header=F ) 
-colnames(groups) <- c("Cohort", "SampleSize") 
 groups$GenomicInflationTech <- 0 
 groups$GenomicInflationPerm <- 0 
 
-oFile <- "UCLex_October_all_samples_model_comparison.pdf"
+oFile <- "UCLex_October_big_cohorts_model_comparison.pdf"
 pdf(oFile)
 par(mfrow=c(2,2), cex.main = 0.6) 
 
@@ -36,27 +18,19 @@ par(mfrow=c(2,2), cex.main = 0.6)
 		{
 		file <- read.table(permFile , header=T)		
 		pvals=pchisq(file$Perm_1,1,lower=F)*0.5
-		#gi <- qq.chisq(-2*log(as.numeric(as.character(pvals))), df=2, x.max=30, main = paste(batches[i], "Perm"), pvals=T)
-		
-		gi <- estlambda(file$Perm_1, df =2 , plot=T, main = paste(batches[i], "Perm")) $estimate
+		GI <- qq.chisq(-2*log(as.numeric(as.character(pvals))), df=2, x.max=30, main = paste(batches[i], "Perm"), pvals=T) ; message(GI[3][[1]])
 		hit <- grep(batches[i], groups[,1] ) 		
-		groups$GenomicInflationPerm[hit] <- gi[[3]]
+		groups$GenomicInflationPerm[hit] <- GI[3][[1]]
 		}
 
 		techFile <- paste0(files[i], "/regressALL")
 		if(file.exists(techFile)) 
 		{	
 		file <- read.table(techFile , header=T)		
-		pvals <- as.numeric(as.character(file$LRT_P_Perm))
-
-		#gi <-	qq.chisq(-2*log(pvals), df=2, x.max=30, main = paste(batches[i], "Tech"), pvals=T)
-
-		gi <- estlambda(file$Perm_1, df =2 , plot=T, main = paste(batches[i], "Tech") )$estimate
+		GI <- qq.chisq(-2*log(as.numeric(as.character(file$LRT_P_Perm))), df=2, x.max=30, main = paste(batches[i], "Tech"), pvals=T); message(GI[3][[1]])
 		hit <- grep(batches[i], groups[,1] ) 		
-		groups$GenomicInflationTech[hit] <- gi[[3]]
+		groups$GenomicInflationTech[hit] <- GI[3][[1]]
 		}
 	}	
 
 dev.off()
-
-groups <- groups[order(groups$GenomicInflationPerm),]
