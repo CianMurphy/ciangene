0ab9598bca52030af627c9f78249ae1ac833d89d
Cian Murphy <cian.murphy.11@ucl.ac.uk>
2015-02-26 17:55:52 +0000

    Added some SingleVariant tests

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 git add -u
-git commit -m "Updating step1"
+git commit -m "Added some SingleVariant tests"
 git push -u origin master
 
---
+++ scripts/prepare_kinship_matrix_for_fastLMM.R
@@ -0,0 +1,15 @@
+release <- 'February2015'
+
+bDir <- paste0("/scratch2/vyp-scratch2/cian/UCLex_", release, "/") 
+
+kin=as.matrix(read.table(paste0(bDir, "TechKin.grm.raw"))) 
+fam=as.matrix(read.table(paste0(bDir, "allChr_snpStats.fam")))
+fam=as.matrix(read.table(paste0(bDir, "TechKin.grm.id")))
+
+names=paste(fam[,1],fam[,2])
+names2="var";for(i in
+1:nrow(fam)){names2=c(names2,paste(fam[i,1],fam[i,2]))}
+
+write.table(rbind(names2), file = paste0(bDir, "TechnicalKinship_Fastlmm") ,row=F,col=F,quote=F,sep="\t")
+write.table(cbind(names,kin),file = paste0(bDir, "TechnicalKinship_Fastlmm"),row=F,col=F,quote=F,sep="\t",append=T)
+
