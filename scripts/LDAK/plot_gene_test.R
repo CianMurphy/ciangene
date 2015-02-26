dec49387ba7a67a4eae2fa06207a418db2d5b33a
Cian Murphy <cian.murphy.11@ucl.ac.uk>
2015-02-26 16:15:53 +0000

    Updating step1

--- scripts/LDAK/plot_gene_test.R
+++ scripts/LDAK/plot_gene_test.R
@@ -1,25 +1,64 @@
+getArgs <- function() {
+  myargs.list <- strsplit(grep("=",gsub("--","",commandArgs()),value=TRUE),"=")
+  myargs <- lapply(myargs.list,function(x) x[2] )
+  names(myargs) <- lapply(myargs.list,function(x) x[1])
+  return (myargs)
+}
+
+release <- 'February2015'
+
+myArgs <- getArgs()
+
+if ('rootODir' %in% names(myArgs))  rootODir <- myArgs[[ "rootODir" ]]
+if ('release' %in% names(myArgs))  release <- myArgs[[ "release" ]]
+
+######################
+
 library(snpStats)
 
-files <- list.files(pattern = "with_kin") 
-batches <- gsub(files, pattern = ".*01_", replacement = "")
+oDir <- paste0("/scratch2/vyp-scratch2/cian/UCLex_", release, "/") 
 
-groups <- read.table("/cluster/project8/vyp/cian/data/UCLex/UCLex_October2014/All_phenotypes/Nb_per_group", header=F ) 
+files <- list.files(paste0(oDir, "LDAK_gene_tests_all_phenos/"), pattern = "with_kin", full.names=T) 
+batches <- gsub(files, pattern = ".*Func_", replacement = "")
+
+groups <- read.table(paste0(oDir, "cohort.summary"), header=T) 
 groups$GenomicInflationTech <- 0 
 groups$GenomicInflationPerm <- 0 
+groups$GenomicInflationBase <- 0 
 
-oFile <- "UCLex_October_big_cohorts_model_comparison.pdf"
+noKins <- paste0(oDir, "LDAK_gene_tests_all_phenos/no_kin_maf_0.000001_Func_", batches, "/regressALL")
+
+
+oFile <- paste0(oDir, "UCLex_", release, "_gene_tests.pdf") 
 pdf(oFile)
 par(mfrow=c(2,2), cex.main = 0.6) 
 
 	for(i in 1:length(files)) 
 	{
+
+		if(file.exists(noKins[i]))
+		{
+		file <- read.table(noKins[i] , header=T)		
+		GI <- qq.chisq(-2*log(as.numeric(as.character(file$LRT_P_Perm))), df=2, x.max=30, main = paste(batches[i], "Base") ,  pvals=T)
+		legend("topleft", , signif(GI[3][[1]]),2) 
+		out <- gsub(dirname(noKins[i]), pattern = ".*phenos/", replacement = "")
+		message(paste(out, "base GI is", GI[3][[1]] ) ) 
+
+		hit <- grep(batches[i], groups$Cohort ) 		
+		groups$GenomicInflationBase[hit] <- GI[3][[1]]
+		}
+
 		permFile <- paste0(files[i], "/regress1")
 		if(file.exists(permFile))
 		{
 		file <- read.table(permFile , header=T)		
 		pvals=pchisq(file$Perm_1,1,lower=F)*0.5
-		GI <- qq.chisq(-2*log(as.numeric(as.character(pvals))), df=2, x.max=30, main = paste(batches[i], "Perm"), pvals=T) ; message(GI[3][[1]])
-		hit <- grep(batches[i], groups[,1] ) 		
+		GI <- qq.chisq(-2*log(as.numeric(as.character(pvals))), df=2, x.max=30, main = paste(batches[i], "Perm") ,  pvals=T)
+		legend("topleft", , signif(GI[3][[1]]),2) 
+		out <- gsub(dirname(permFile), pattern = ".*phenos//", replacement = "")
+		message(paste(out, "permuted GI is", GI[3][[1]] ) ) 
+
+		hit <- grep(batches[i], groups$Cohort ) 		
 		groups$GenomicInflationPerm[hit] <- GI[3][[1]]
 		}
 
@@ -27,10 +66,17 @@ par(mfrow=c(2,2), cex.main = 0.6)
 		if(file.exists(techFile)) 
 		{	
 		file <- read.table(techFile , header=T)		
-		GI <- qq.chisq(-2*log(as.numeric(as.character(file$LRT_P_Perm))), df=2, x.max=30, main = paste(batches[i], "Tech"), pvals=T); message(GI[3][[1]])
-		hit <- grep(batches[i], groups[,1] ) 		
+		GI <- qq.chisq(-2*log(as.numeric(as.character(file$LRT_P_Perm))), df=2, x.max=30, main = paste(batches[i], "Tech") ,  pvals=T)
+		legend("topleft", , signif(GI[3][[1]]),2) 
+		out <- gsub(dirname(techFile), pattern = ".*phenos//", replacement = "")
+		message(paste(out, "GI is", GI[3][[1]] ) ) 
+
+		hit <- grep(batches[i], groups$Cohort ) 		
 		groups$GenomicInflationTech[hit] <- GI[3][[1]]
 		}
 	}	
 
 dev.off()
+
+
+
