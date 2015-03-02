62fcc93ccbd4d539a5ce56c849668384787c924d
Cian Murphy VYP <sejjcmu@pchuckle.local>
2015-03-02 21:28:49 +0000

    SingleVariantTests

--- scripts/plot_singleVariant_results.R
+++ scripts/plot_singleVariant_results.R
@@ -19,12 +19,12 @@ bDir <- paste0("/scratch2/vyp-scratch2/cian/UCLex_", release, "/")
 
 iDir <- paste0(bDir, "FastLMM_Single_Variant_all_phenos/")
 
-noKin <- list.files(iDir, pattern = "no_kin")
-techKin <- list.files(iDir, pattern = "Tech_kin")
+noKin <- list.files(iDir, pattern = "no_kin", full.names=TRUE)
+techKin <- list.files(iDir, pattern = "Tech_kin", full.names=TRUE)
 
-groups <- gsub(techKin, pattern = "_.*", replacement = "")
+groups <- gsub(basename(techKin) , pattern = "_.*", replacement = "")
 
-annotations <- read.table(paste0(bDir, "annotations.snpStat"), header=T)
+annotations <- read.csv(paste0(bDir, "annotations.snpStat"), header=T, sep="\t")
 extCtrl <- read.table(paste0(bDir, "Ext_ctrl_variant_summary"), header=T )
 extCtrl.small <- data.frame(extCtrl[,1], extCtrl$MAF)
 colnames(extCtrl.small) <-  c("SNP", "ExtCtrl_MAF") 
@@ -45,20 +45,20 @@ for(i in 1:length(groups))
 
 	results.merged <- merge(base, tech, by = "SNP")
 
-	results.merged.anno <- merge(results.merged, annotations, by.x = "SNP", by.y "clean.signature")
+	results.merged.anno <- merge(results.merged, annotations, by.x = "SNP", by.y ="clean.signature")
 
 	results.merged.anno.extCtrl <- merge(results.merged.anno, extCtrl.small, by = "SNP")
 
 	write.table(results.merged.anno.extCtrl, paste0(iDir, groups[i], "_filt"), col.names=T, row.names=F, quote=F, sep="\t")
 
 
-	lapply(minMaf) function(x)
+	lapply(minMaf) (x)
 	{
 		dat <- subset(results.merged.anno.extCtrl, results.merged.anno.extCtrl$ExtCtrl_MAF >= ExtCtrl_MAF)
 		qq.chisq(-2*log(dat$Pvalue), df=2, x.max=30, pvals=T, main = paste(groups[i], x, "noKin"))
 		qq.chisq(-2*log(dat$TechKinPvalue), df=2, x.max=30, pvals=T, main = paste(groups[i], x, "TechKin"))
 	}
-	)
+	
 
 }
 dev.off()
